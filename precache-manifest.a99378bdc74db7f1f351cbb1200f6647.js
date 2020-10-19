1d692adb3986ad03c9c7e2d6edc8f8269ad97700
priya-tsa <priyaayyori.tsa@gmail.com>
2020-10-19 16:26:34 +1100

    template

---
+++ precache-manifest.a99378bdc74db7f1f351cbb1200f6647.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dda188f2781d4c1fc24aa8f53598d08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daebdb8f259712fe8fd7",
+    "url": "/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "566ee5344d162ecdfb85",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "daebdb8f259712fe8fd7",
+    "url": "/static/js/2.e1eec22d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.e1eec22d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "566ee5344d162ecdfb85",
+    "url": "/static/js/main.dd9f0d9c.chunk.js"
+  },
+  {
+    "revision": "991106e2a668dd08aca3",
+    "url": "/static/js/runtime-main.434d0006.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
